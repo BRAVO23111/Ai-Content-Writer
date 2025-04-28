9ade53cb6111c6e600858447df2fbc18ad8aaabb
DEBANJAN MUKHERJEE <debanjanmukherjee015@gmail.com>
2025-04-28 16:09:04 +0530

    last bugs

--- prisma/src/generated/prisma/edge.js
+++ prisma/src/generated/prisma/edge.js
@@ -161,11 +161,12 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgresql://neondb_owner:npg_dhDHCsxX0gl8@ep-ancient-water-a4fcd0f9-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require"
+        "value": null
       }
     }
   },
