2b4e574a5d0ec60fe0406d16ce578c247adb8e99
DEBANJAN MUKHERJEE <debanjanmukherjee015@gmail.com>
2025-05-20 12:20:54 +0530

    bugs

--- app/api/auth/twitter/callback/route.ts
+++ app/api/auth/twitter/callback/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import { cookies } from 'next/headers';
-import { getAccessToken } from '../../../../../lib/twitteroauth';
+import { getAccessToken } from '@/lib/twitteroauth';
 
 
 export async function GET(request: Request) {
--- app/api/auth/twitter/route.ts
+++ app/api/auth/twitter/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 
 import { cookies } from 'next/headers';
 import crypto from 'crypto';
-import { getAuthorizationUrl } from '../../../../lib/twitteroauth'
+import { getAuthorizationUrl } from '@/lib/twitteroauth'
 
 export async function GET() {
   try {
